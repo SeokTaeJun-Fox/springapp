8edb436fbcc9fb311b4516dd71a8584feb3eeccb
김세종 <oasis0031@gmail.com>
2026-05-21 10:31:25 +0900

    feat.GuestbookVO들 추가

---
+++ src/main/java/com/app/springapp/domain/vo/GuestbookReplyVO.java
@@ -0,0 +1,16 @@
+package com.app.springapp.domain.vo;
+
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class GuestbookReplyVO {
+
+   private Long id;
+   private String guestbookReplyContent;
+   private String guestbookReplyCreatedAt;
+   private Long guestbookId;
+   private Long memberId;
+}
