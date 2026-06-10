b9cea14efccc65e1534b6c964d2d278a083d5ae8
smileman0511 <kangdh010511@gmail.com>
2026-06-11 01:16:09 +0900

    feat: 프로젝트 처음 생성시 제안 추천하기

--- src/main/java/com/app/springapp/service/SuggestionService.java
+++ src/main/java/com/app/springapp/service/SuggestionService.java
@@ -12,4 +12,7 @@ public interface SuggestionService {
 
     // 프로젝트별 제안 목록 조회
     List<SuggestionResponseDTO> getSuggestionsByProjectId(Long projectId);
+
+    // 프로젝트 생성 초기 - AI가 선별한 추천 제안 최대 3개 반환
+    List<SuggestionResponseDTO> getRecommendedSuggestions(Long projectId);
 }
